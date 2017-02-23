ca43af290bf9d3d1bbf8de92bf7387d21cc710d1
Thomas Weinert <thomas@weinert.info>
2017-02-23 18:50:58 +0100

    Add comments to the implementation

--- counter.c
+++ counter.c
@@ -1,11 +1,16 @@
 #include "php_counter.h"
 
-#define COUNTER_NS "counter"
+#define COUNTER_NS "Counter"
 
+/* Declare global module variable */
 ZEND_DECLARE_MODULE_GLOBALS(counter)
+
+
+/* START: function implementations Counter\clear() */
 
 PHP_FUNCTION(counter_get)
 {
+    /* COUNTER_G() template is defined in php_counter.h */
 	RETURN_LONG(COUNTER_G(basic_counter_value));
 }
 
@@ -19,6 +24,9 @@ PHP_FUNCTION(counter_clear)
 	COUNTER_G(basic_counter_value) = 0;
 }
 
+/* END: function implementations Counter\clear() */
+
+/* Create module function list */
 const zend_function_entry php_counter_functions[] = {
 	ZEND_NS_NAMED_FE(COUNTER_NS, get, ZEND_FN(counter_get), NULL)
 	ZEND_NS_NAMED_FE(COUNTER_NS, bump, ZEND_FN(counter_bump), NULL)
@@ -26,12 +34,14 @@ const zend_function_entry php_counter_functions[] = {
 	PHP_FE_END
 };
 
+/* Define a request initialization function */
 PHP_RINIT_FUNCTION(counter)
 {
 	COUNTER_G(basic_counter_value) = 0;
 	return SUCCESS;
 }
 
+/* Create the module (extension) entry */
 zend_module_entry counter_module_entry = {
 	STANDARD_MODULE_HEADER,
 	PHP_COUNTER_EXTNAME,
